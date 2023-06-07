c6b40e25b200b84a0695db94aa520d92963aeee6
dora5n <Moe4dora5n@gmail.com>
2023-06-07 19:03:03 +0900

    画像とcss追加

--- js/app.js
+++ js/app.js
@@ -31,23 +31,27 @@ if (random === 0) {
   console.log("大吉です");
   $(".a").html("大吉です");
   $(".a").css("color", "red");
-  $(".a").html("<img src='../img/1.png' >");
-  // $(".a").html("<img src='img/1.png' >")
+  $(".a").html("<img src='../img/1.jpg' >");
+  // $(".a").html("<img src='img/1.jpg' >")
 } else if (random === 1) {
   console.log("中吉です");
   $(".a").html("中吉です");
   $(".a").css("color", "green");
+  $(".a").html("<img src='../img/2.jpg' >");
 } else if (random === 2) {
-  console.log("小吉です");
-  $(".a").html("小吉です");
-  $(".a").css("color", "yellow");
-} else if (random === 3) {
   console.log("吉です");
   $(".a").html("吉です");
+  $(".a").css("color", "yellow");
+  $(".a").html("<img src='../img/4.jpg' >");
+} else if (random === 3) {
+  console.log("凶です");
+  $(".a").html("凶です");
   $(".a").css("color", "blue");
+  $(".a").html("<img src='../img/5.jpg' >");
 } else if (random === 4) {
-  console.log("末です");
-  $(".a").html("末です");
+  console.log("大凶です");
+  $(".a").html("大凶です");
   $(".a").css("color", "black");
+  $(".a").html("<img src='../img/6.jpg' >");
 }
 });
